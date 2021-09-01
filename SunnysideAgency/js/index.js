40d4e16100f05b0fe5e9f88bfdb995fad752c661
Kevin Garcia <kevinede40@gmail.com>
2021-09-01 15:40:48 -0600

    menu function

--- SunnysideAgency/js/index.js
+++ SunnysideAgency/js/index.js
@@ -1,7 +1,22 @@
-let menu = document.getElementById('menu')
+const menuuu = document.getElementById('menu')
+const botton = document.getElementById('displayMenu')
+console.log(menuuu.style.display)
 
-function displayMenu(){
+
+/* function displayMenu(){
     if(menu.style.display == 'none'){
-        menu.style.display = 'flex'
+        menu.style.display == 'flex'
     }
-}
+} */
+/* botton.addEventListener('click', function(){
+    
+    menuuu.classList.remove('status_1');
+    menuuu.classList.add('status_2');
+}) */
+botton.addEventListener('click', function(){
+    
+    menuuu.classList.toggle('status_1');
+    menuuu.classList.toggle('status_2');
+})
+
+console.log(menuuu.style.display)
